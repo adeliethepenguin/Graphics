52e82f0737af4f9b55ed4c6b72e3300e5401a0dd
adeliethepenguin <116315307+adeliethepenguin@users.noreply.github.com>
2023-09-29 14:45:24 -0400

    Graphics
    Graphics
    Graphics
    Graphics

---
+++ Game.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace Graphics 
+{
+    internal class Game : GameWindow
+    {
+        public Game(int width, int height, string title): base(GameWindowSettings.Default, new NativeWindowSettings() { Title = title, Size = (width, height)})
+        {
+
+        }
+
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+
+            GL.ClearColor(1, 0, 1, 0);
+        }
+
+        protected override void OnUnload()
+        {
+            base.OnUnload();
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            base.OnRenderFrame(args);
+
+            // MUST BE THE FIRST
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+
+            // MUST BE THE LAST
+            SwapBuffers();
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+
+            if (KeyboardState.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Graphics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Game myGame = new Game(800, 700, "Hire this man!"))
+            {
+                myGame.Run();
+            }
+        }
+    }
+}
